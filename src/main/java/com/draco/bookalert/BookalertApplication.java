3ffb40934887e5e5d3fcbc12b514710dabf4efb5
Anthony Villegas <villegasanthony87@yahoo.com>
2021-11-16 12:30:02 -0600

    feat: initial commit dependency setup

---
+++ src/main/java/com/draco/bookalert/BookalertApplication.java
@@ -0,0 +1,13 @@
+package com.draco.bookalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookalertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookalertApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/draco/bookalert/BookalertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.draco.bookalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookalertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
