bec603e048e4a25945abc1e5b7f6698156c65fd5
Christina Nuncio <nuncio.christina01@gmail.com>
2021-11-22 12:26:47 -0600

    no changes

--- src/main/java/com/draco/bookalert/controllers/HomeController.java
+++ src/main/java/com/draco/bookalert/controllers/HomeController.java
@@ -33,4 +33,8 @@ public class HomeController {
         return "users/about";
     }
 
+//    @GetMapping("/profile")
+//    public String profilePage() {
+//        return "users/profile";
+//    }
 }
