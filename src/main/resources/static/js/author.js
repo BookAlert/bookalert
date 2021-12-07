a86a7ccc2fa040c3841f487e5f754c9561340188
Javier Guerra <javier.j.guerra4@gmail.com>
2021-12-07 15:47:50 -0600

    Purchased button returns a staus id of three

--- src/main/resources/static/js/author.js
+++ src/main/resources/static/js/author.js
@@ -10,4 +10,16 @@ $(() => {
         })
     })
     console.log($(this).data('book-id'))
+
+    $('body').on('click', '.mark-purchased', function () {
+        fetch("/authors/mark-purchased", {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify({id: $(this).data('book-id') })
+        })
+    })
+    console.log($(this).data('book-id'))
 })
