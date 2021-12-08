e0f47363a1b687862d57be6113332d3da449d0bd
Anthony Villegas <villegasanthony87@yahoo.com>
2021-12-08 00:08:44 -0600

    slick library added for carousel on home page

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -1,5 +1,23 @@
 $(() => {
 
+    /// Carousel Handler
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true
+    });
+
+
     ///=================  FETCH DATA FOR AUTHOR
     $('body').on('click', '#purchase', function () {
         console.log("testing");
