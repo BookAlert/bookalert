41815a532640bbb00b21abbac99044198d665df0
Javier Guerra <javier.j.guerra4@gmail.com>
2021-12-10 11:48:19 -0600

    updated author js

--- src/main/resources/static/js/author.js
+++ src/main/resources/static/js/author.js
@@ -1,4 +1,5 @@
 $(() => {
+    //==========SAVE-BOOK HANDLER
     $('body').on('click', '.save-book', function () {
         fetch("/authors/save-book", {
             headers: {
@@ -6,11 +7,25 @@ $(() => {
                 'Content-Type': 'application/json'
             },
             method: "POST",
-            body: JSON.stringify({id: $(this).data('book-id') })
+            body: JSON.stringify({id: $(this).data('book-id')})
+        }).then(() => {
+            iziToast.success({
+                title: 'Success',
+                message: 'Successfully saved book!',
+                position: 'topRight',
+                timeout: 3000
+            });
+        }).catch(() => {
+            iziToast.fail({
+                title: 'Failure',
+                message: 'Book exists!',
+                position: 'center',
+                timeout: 3000
+            })
         })
     })
-    console.log($(this).data('book-id'))
-
+    // console.log($(this).data('book-id'))
+    //==========MARK PURCHASED HANDLER
     $('body').on('click', '.mark-purchased', function () {
         fetch("/authors/mark-purchased", {
             headers: {
@@ -18,10 +33,30 @@ $(() => {
                 'Content-Type': 'application/json'
             },
             method: "POST",
-            body: JSON.stringify({id: $(this).data('book-id') })
+            body: JSON.stringify({id: $(this).data('book-id')})
+        }).then(() => {
+            iziToast.info({
+                title: 'Purchased',
+                message: 'You own this book',
+                position: 'topLeft',
+                timeout: 5000
+            });
+        }).catch(() => {
+            iziToast.fail({
+                title: 'Failure',
+                message: 'Failed',
+                position: 'center',
+                timeout: 3000
+            })
         })
+        // console.log($(this).data('book-id'))
     })
-    console.log($(this).data('book-id'))
+
+    $(function(){
+        $("#purchase-button").on("click", function(){
+            $("#purchased-text").css("visibility", "visible");
+        });
+    })
 
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
